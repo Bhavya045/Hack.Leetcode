7137a6325e33c05d1d79b03122120fb473affcd1
Bhavya045 <backupstorage114@gmail.com>
2025-06-26 07:18:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -3,22 +3,32 @@ class Solution {
         if(hand.length%groupSize!=0){
             return false;
         }
-        TreeMap<Integer,Integer>map=new TreeMap<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<hand.length;i++){
             map.put(hand[i],map.getOrDefault(hand[i],0)+1);
         }
-        while(map.size()>0){
-            int currentcard=map.entrySet().iterator().next().getKey();
-            for(int i=0;i<groupSize;i++){
+        //while(map.size()>0){
+           // int currentcard=map.entrySet().iterator().next().getKey();
+
+            for(int carde:hand){
+                if(!map.containsKey(carde)){
+                    continue;
+                }
+                int  currentcard=carde;
+                while(map.containsKey( currentcard-1)){
+                     currentcard--;
+                }
+              for(int i=0;i<groupSize;i++){
                // currentcard+i, currentcard+i, currentcard+i,
                int card= currentcard+i;
                if(!map.containsKey(card)) return false;
                map.put(card,map.get(card)-1);
                if(map.get(card)==0){
                 map.remove(card);
+                  }
+               }
             }
-            }
-        }
+        
         return true;
     }
 }
