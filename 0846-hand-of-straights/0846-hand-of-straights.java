635c318ae96c9d0005df63bdcb6c984b3f692749
Bhavya045 <backupstorage114@gmail.com>
2025-08-12 00:53:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,34 +1,14 @@
 class Solution {
-    public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length%groupSize!=0){
-            return false;
-        }
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<hand.length;i++){
-            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
-        }
-        //while(map.size()>0){
-           // int currentcard=map.entrySet().iterator().next().getKey();
-
-            for(int carde:hand){
-                if(!map.containsKey(carde)){
-                    continue;
-                }
-                int  currentcard=carde;
-                while(map.containsKey( currentcard-1)){
-                     currentcard--;
-                }
-              for(int i=0;i<groupSize;i++){
-               // currentcard+i, currentcard+i, currentcard+i,
-               int card= currentcard+i;
-               if(!map.containsKey(card)) return false;
-               map.put(card,map.get(card)-1);
-               if(map.get(card)==0){
-                map.remove(card);
-                  }
-               }
+     class Card{
+            int card;
+            int count;
+            public Card(int card, int count){
+                this.card = card;
+                this.count= count;
             }
+        }
         
-        return true;
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+       if(isNStraightHand%groupSize !0)return false
     }
 }
