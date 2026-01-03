1d24afd9262ae7d5bd40d06e67808d4712dd4970
Bhavya045 <backupstorage114@gmail.com>
2026-01-03 15:43:47 +0530

    Time: 5 ms (40.52%), Space: 43.5 MB (55.51%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -2,17 +2,18 @@
     public String removeOuterParentheses(String s) {
         StringBuilder sb = new StringBuilder();
         int opened = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                if (++opened > 1) {
-                    sb.append(c);
-                }
-            } else {
-                if (--opened > 0) {
-                    sb.append(c);
-                }
-            }
+       for(int i=0; i<s.length();i++){
+        if(s.charAt(i) == ')'){
+            opened--;
         }
+        if(opened!=0){
+            sb.append(s.charAt(i));
+        }
+         if(s.charAt(i) == '('){
+            opened++;
+        }
+
+       }
         return sb.toString();
     }
 }
